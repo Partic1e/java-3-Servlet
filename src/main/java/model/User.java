265994895937781ce125ce6c94b04407155f96be
Igor <IgorLom2003@mail.ru>
2024-05-13 10:54:14 +0500

    7

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,15 +1,32 @@
 package model;
 
-public class User
-{
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "login", unique = true, updatable = false)
     private String login;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "email")
     private String email;
 
     public User(String login, String password, String email) {
         this.login = login;
         this.password = password;
         this.email = email;
+    }
+
+    public User() {
+
     }
 
     public String getLogin() {
